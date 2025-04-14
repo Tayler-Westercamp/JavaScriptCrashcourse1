6d98ad6313c7960a88e735142825394e43d38f04
Tayler <tayler.westercamp@gmail.com>
2025-04-14 18:27:36 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log('Hello world'[10])
+console.log(''.length)
+let str = 'Tayler'
+console.log(str[str.length-1])
+
+let isRaining = true
+let temperature = 20
+
+temperature = temperature + 2
+
+console.log(temperature)
+
+let celcius = 10;
+let fahrenheit = (celcius * 1.8 + 32)
+
+console.log(fahrenheit)
